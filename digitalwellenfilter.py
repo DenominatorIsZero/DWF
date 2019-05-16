9e7abccf1b4992524a080c55e4409ba8b6cdb11f
Dameji <trueiggi@web.de>
2019-05-16 15:16:24 +0200

    initial commit

---
+++ digitalwellenfilter.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Element:
+    """
+    Einzelelement fuer einen Digitalwellenfilter
+    Typ 1 fuer 0 > gamma > -0.5
+    Typ 2 fuer -0.5 > gamma > -1
+    """
+    def __init__(self, a, typ=1):
+        self.a = a
+        self.delay = np.zeros(2)
+        self.ein = 0
+        self.aus = 0
+        if typ in {1, 2}:
+            self.typ = typ
+        else:
+            raise Exception("Ungueltiger Typ '{}'".format(self.typ))
+
+    def advance(self):
+        if self.typ == 1:
+            s1 = self.ein + self.delay[1]
+            s2 = s1 * self.a - self.delay[1]
+            s3 = s1 + s2
+
+            self.aus = s2
+            self.delay[1] = self.delay[0]
+            self.delay[0] = s3
+
+        elif self.typ == 2:
+            s1 = self.ein + self.delay[1]
+            s2 = s1 * self.a - self.delay[1]
+            s3 = s2 - s1
+
+            self.aus = s3
+            self.delay[1] = self.delay[0]
+            self.delay[0] = s2
+        else:
+            raise Exception("Ungueltiger Typ '{}'".format(self.typ))
+
+
+class Filter():
+    """
+    Digitalwellenfilter 19. Grades mit Grenzfrequenz fg.
+    Ausgang 1: Tiefpass
+    Ausgang 2: Hochpass
+    """
+    def __init__(self):
+        self.e = [Element(0, 1)]
+
+        self.e.append(Element(0.226119, 1))
+        self.e.append(Element(0.397578, 2))
+        self.e.append(Element(0.160677, 2))
+        self.e.append(Element(0.049153, 2))
+
+        self.e.append(Element(0.063978, 1))
+        self.e.append(Element(0.423068, 1))
+        self.e.append(Element(0.258673, 2))
+        self.e.append(Element(0.094433, 2))
+        self.e.append(Element(0.015279, 2))
+
+        self.aus = 0
+        self.ein = 0
+        self.delay = 0
+
+    def advance(self):
+        self.e[1].ein = self.ein
+        self.e[5].ein = self.ein
+        self.e[1].advance()
+        self.e[5].advance()
+
+        self.e[2].ein = self.e[1].aus
+        self.e[6].ein = self.e[5].aus
+        self.e[2].advance()
+        self.e[6].advance()
+
+        self.e[3].ein = self.e[2].aus
+        self.e[7].ein = self.e[6].aus
+        self.e[3].advance()
+        self.e[7].advance()
+
+        self.e[4].ein = self.e[3].aus
+        self.e[8].ein = self.e[7].aus
+        self.e[4].advance()
+        self.e[8].advance()
+
+        self.delay = self.e[4].aus
+        self.e[9].ein = self.e[8].aus
+        self.e[9].advance()
+
+        self.aus = 0.5 * (self.delay - self.e[9].aus)
+        # # Ausgang:
+        # self.aus = 0.5 * (self.delay - self.e[9].aus)
+        # # Oberer Zweig:
+        # self.delay = self.e[4].aus
+        # self.e[4].ein = self.e[3].aus
+        # self.e[3].ein = self.e[2].aus
+        # self.e[2].ein = self.e[1].aus
+        # self.e[1].ein = self.ein
+        # # Unterer Zweig:
+        # self.e[9].ein = self.e[8].aus
+        # self.e[8].ein = self.e[7].aus
+        # self.e[7].ein = self.e[6].aus
+        # self.e[6].ein = self.e[5].aus
+        # self.e[5].ein = self.ein
+        # # Advance the Elements
+        # for e_ in self.e:
+        #     e_.advance()
+
+
+def test_Filter():
+    n = 2**14
+    x = np.zeros(n)
+    y = np.zeros(n)
+    Fs = 64e3
+    delta_f = Fs/n
+    fscale = np.arange(0, Fs/2, delta_f)
+    x[0] = 1
+
+    f = Filter()
+
+    for index in range(n):
+        f.ein = x[index]
+        f.advance()
+        y[index] = f.aus
+
+    plt.figure(1).set_size_inches(12, 8)
+    plt.title("Digitalwellenfilter Zeitbereich")
+    plt.plot(x)
+    plt.plot(y)
+    plt.figure(2).set_size_inches(12, 8)
+    plt.title("Digitalwellenfilter Frequenzbereich")
+    plt.plot(fscale, -20 * np.log10(abs(np.fft.fft(y)[0:n//2])))
+    plt.show()
+
+
+def test_fft():
+    Fs = 64e3
+    f1 = 16e3
+    f2 = 3e3
+    f3 = 31e3
+    x = np.zeros(2048)
+    y = np.array(range(2048))
+    x = (np.sin(2 * np.pi * y * f1 / Fs) + np.sin(2 * np.pi * y * f2 / Fs) +
+         np.sin(2 * np.pi * y * f3 / Fs))
+
+    plt.figure(1)
+    plt.plot(x)
+    plt.figure(2)
+    plt.plot(abs(np.fft.fft(x)))
+    plt.show()
+
+
+test_Filter()
---
+++ test_dwf.py
@@ -0,0 +1,53 @@
+import digitalwellenfilter as dwf
+
+
+def test_Element1_init():
+    e = dwf.Element(1, 1)
+    assert e.a == 1
+    assert e.delay[0] == 0
+    assert e.delay[1] == 0
+    assert e.ein == 0
+    assert e.aus == 0
+    assert e.typ == 1
+
+
+def test_Element1_advance():
+    a = 0.333
+    e = dwf.Element(a, 1)
+    e.ein = 1
+    e.advance()
+    assert e.aus == a
+    assert e.delay[0] == 1.333
+    assert e.delay[1] == 0
+
+    e.ein = 0
+    e.advance()
+    assert e.aus == 0
+    assert e.delay[0] == 0
+    assert e.delay[1] == 1.333
+
+    e.advance()
+    assert e.aus == 1.333 * a - 1.333
+    assert e.delay[0] == 1.333 + a * 1.333 - 1.333
+    assert e.delay[1] == 0
+
+
+def test_Element2_advance():
+    a = 0.333
+    e = dwf.Element(a, 2)
+    e.ein = 1
+    e.advance()
+    assert e.aus == -1 + a * 1
+    assert e.delay[0] == a * 1
+    assert e.delay[1] == 0
+
+    e.ein = 0
+    e.advance()
+    assert e.aus == 0
+    assert e.delay[0] == 0
+    assert e.delay[1] == a * 1
+
+    e.advance()
+    assert e.aus == -a + (-1 + a) * a
+    assert e.delay[0] == -a + a*a
+    assert e.delay[1] == 0
